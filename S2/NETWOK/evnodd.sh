59734b7a4275370745952f2fb6f1e6616b989861
Aswin G Nair <150990824+aswingnair2002@users.noreply.github.com>
2024-03-18 15:41:09 +0530

    Delete S2/NETWOK directory

--- S2/NETWOK/evnodd.sh
+++
@@ -1,8 +0,0 @@
-echo "Enter the number: "
-read x
-if [ $(($x%2)) == 0 ]
-then
-	echo "The number is even"
-else
-	echo "The number is odd"
-fi
--- S2/NETWOK/factorial.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Enter the Number : "
-read x
-fact=1
-for ((i=$x; i>=1 ; i--))
-do
-	fact=$(($fact*$i))
-done
-echo "Factorial = $fact"
